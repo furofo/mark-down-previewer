388482dc6ab23a849a18fa6442f0cf060d07626c
furofo <chriscellphonerepair@gmail.com>
2020-05-20 20:31:31 -0400

    added first render for react app

--- src/index.js
+++ src/index.js
@@ -4,3 +4,19 @@ import './style.css';
 import $ from "jquery";
 
 console.log('this is a test');
+
+class MarkDownContainer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+        <div>
+        <p>Hello World!</p>
+        </div>
+        );
+    }
+
+}
+
+ReactDom.render(<MarkDownContainer />, document.getElementById('root'));
