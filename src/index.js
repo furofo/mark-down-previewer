ebd9a84644043fbbf0fff3121588809d8b52a079
furofo <chriscellphonerepair@gmail.com>
2020-05-22 20:39:13 -0400

    initiated test with full markdown code

--- src/index.js
+++ src/index.js
@@ -6,15 +6,59 @@ import marked from 'marked';
 
 console.log('this is a test');
 
-let placeholderText = `# Welcome to my React Markdown Previewer!
-`;
+let placeholderText =  `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`
 
 class MarkDownContainer extends React.Component {
     constructor(props) {
         super(props);
     }
     componentDidMount() {
-        document.getElementById('preview-code').innerHTML = marked('# Welcome to my React Markdown Previewer!');
+        document.getElementById('preview-code').innerHTML = marked(placeholderText);
     }
     render() {
         return(
