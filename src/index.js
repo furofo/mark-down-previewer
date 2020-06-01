a7dcb7334ae74e954b0d683c892342768957bdd2
furofo <chriscellphonerepair@gmail.com>
2020-06-01 02:46:05 -0400

    cleaned up code final connecting redux to react

--- src/index.js
+++ src/index.js
@@ -9,111 +9,8 @@ import { connect } from 'react-redux';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux';
 
+//global variables
 
-
-
-const reducer =  function (state = {input: 'hello', previewSwitch: true, editorSwitch: true,}, action)  {
-    if(action.type == "message"){
-        return {
-            input: action.input,
-            previewSwitch: state.previewSwitch,
-            editorSwitch: state.editorSwitch,
-        }
-    }
-    else if (action.type == "previewToggle") {
-        return {
-            input: state.input,
-            previewSwitch: !state.previewSwitch,
-            editorSwitch: state.editorSwitch,
-        }
-    }
-    else if (action.type == "editorToggle") {
-        return {
-            input: state.input,
-            previewSwitch: state.previewSwitch,
-            editorSwitch: !state.editorSwitch,
-        }
-    }
-
-
-    else {
-    return state;
-    }
-  }
-  
-  // three things in react state right input, preview switch, and editor switch
-
-
-//Redux Logic
-
-let store = createStore(reducer);
-
-console.log('this is store state');
-console.log(store.getState());
-
-
-
-
-const loginAction = function (message) {
-    return {
-      type: 'message',
-      input: message,
-    }
-  };
-  const previewToggle = () => {
-    return {
-      type: 'previewToggle',
-     
-    }
-  };
-  const editorToggle = () => {
-    return {
-      type: 'editorToggle',
-    }
-  };
-  
-    store.dispatch(loginAction("hello there world"));
-    console.log('this is store state now');
-      
-    console.log(store.getState());
-    console.log("anddd now");
-    store.dispatch(previewToggle());
-    console.log(store.getState());
-    console.log("anddd finally");
-    store.dispatch(editorToggle());
-    console.log(store.getState());
-
-  
-const mapStateToProps = (state) => {
-        return {
-          input: state.input,
-          previewSwitch: state.previewSwitch,
-          editorSwitch: state.editorSwitch,
-        }
-      };
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        loginAction: (message) => {
-            dispatch(loginAction(message));
-        },
-        previewToggle: () => {
-            dispatch(previewToggle());
-        },
-        editorToggle: () => {
-            dispatch(editorToggle());
-        }
-    }
-}
-      
-
-
-
-//mark up special flags
-
-marked.setOptions({
-    breaks: true
-});
 let placeholderText =  `# Welcome to my React Markdown Previewer!
 
 ## This is a sub-heading...
@@ -161,37 +58,76 @@ And here. | Okay. | I think we get it.
 ![React Logo w/ Text](https://goo.gl/Umyytc)
 `;
 
+//redux logic
+
+const reducer =  function (state = {input: placeholderText, previewSwitch: true, editorSwitch: true,}, action)  {
+    if(action.type == "message"){
+        return {
+            input: action.input,
+            previewSwitch: state.previewSwitch,  // simple logic here if type is message change message and return everything else unchanged
+            editorSwitch: state.editorSwitch,
+        }
+    }
+    else if (action.type == "previewToggle") {
+        return {
+            input: state.input,
+            previewSwitch: !state.previewSwitch,
+            editorSwitch: state.editorSwitch, // these two just siwtch boolean value for either previe or editor switch which changes the menu sizes
+        }
+    }
+    else if (action.type == "editorToggle") {
+        return {
+            input: state.input,
+            previewSwitch: state.previewSwitch,
+            editorSwitch: !state.editorSwitch,
+        }
+    }
+    else {
+    return state;
+    }
+  }
+let store = createStore(reducer);
+
+//these are action creators
+const loginAction = function (message) {
+    return {
+      type: 'message',
+      input: message,
+    }
+  };
+  const previewToggle = () => {
+    return {
+      type: 'previewToggle',
+     
+    }
+  };
+  const editorToggle = () => {
+    return {
+      type: 'editorToggle',
+    }
+  };
+    
+//mark up special flags to pass optional test
+marked.setOptions({
+    breaks: true
+});
 class MarkDownContainer extends React.Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            input: placeholderText,
-            previewSwitch: true,
-            editorSwitch: true,
-        }
-        this.updateMessage = this.updateMessage.bind(this);
+        this.updateMessage = this.updateMessage.bind(this); // this binds this XD so these refer to mark down conatier rather than window object
         this.previewExpand = this.previewExpand.bind(this);
         this.editorExpand = this.editorExpand.bind(this);
 
     }
-
     updateMessage(event) {
-        
-        this.setState({
-            input: event.target.value
-        })
-
-        
+        this.props.loginAction(event.target.value) // uses props it gets from Redux that represent redux state which are defined in my react-redux section
     }
 
-    previewExpand(){
-    
+    previewExpand(){ // logic for hiding and expanding editor and preview
         if(this.props.previewSwitch){
             $(".editor-container").hide("slow", function(){});
             this.props.previewToggle();
         }
-
         else {
             $(".editor-container").show("slow", function(){});
             this.props.previewToggle();
@@ -203,14 +139,11 @@ class MarkDownContainer extends React.Component {
             $(".editor-container").animate({
                 width: "80%",
             }, 1000);
-            
             this.props.editorToggle();
-            //$("#editor").attr("rows", "40");
             $("#editor").animate({
                 rows: "40"
             }, 1000);
         }
-
         else {
             $(".preview-container").show("slow", function(){$(".editor-container").css("width", "70%");});
             $(".editor-container").animate({
@@ -223,22 +156,14 @@ class MarkDownContainer extends React.Component {
             this.props.editorToggle();
         }
     }
-    componentDidMount() {
-        document.getElementById('preview').innerHTML = marked(this.state.input);
-        console.log("what is this");
-       
-        
-        
+    componentDidMount() { // this is called after render so makes sure everything is loaded before this is run do this way so i can wait for props to load since it reads props 
+        document.getElementById('preview').innerHTML = marked(this.props.input); // and otherwise would cause errors
         $("#preview-expander").click(this.previewExpand);
-       
-        $("#editor-expander").click(this.editorExpand);
-       
-        
-        
+        $("#editor-expander").click(this.editorExpand); 
     }
 
     componentDidUpdate() {
-        document.getElementById('preview').innerHTML = marked(this.state.input);
+        document.getElementById('preview').innerHTML = marked(this.props.input); // anytime document updates this gets called so constantly changes input based on this
     }
     render() {
         return(
@@ -246,27 +171,20 @@ class MarkDownContainer extends React.Component {
         <div className = "center">
             <div className = "editor-container">
                 <div id = "editor-heading"> 
-                
-                <i className="fas fa-fire">  Editor</i>
-              <a href ="#" id = "editor-expander">  <i className="fas fa-arrows-alt"></i> </a>
-                
+                    <i className="fas fa-fire">  Editor</i>
+                    <a href ="#" id = "editor-expander">  <i className="fas fa-arrows-alt"></i> </a>
                 </div>
-            
-            
                 <textarea onChange = {this.updateMessage} id = "editor" rows = "15">
-                    {this.state.input}
+                    {this.props.input}
                 </textarea>
             </div>
         </div>
         <div className = "preview-container">
         <div className = "center">
-        <div id = "preview-header"> 
-                
+        <div id = "preview-header">     
                 <i className="fas fa-fire">  Preview</i>
                 <a href = "#" id = "preview-expander"><i className="fas fa-arrows-alt"></i></a>
-                
-                </div>
-            
+                </div> 
         </div>
         <div className = "center">
             <div id = "preview">
@@ -276,13 +194,31 @@ class MarkDownContainer extends React.Component {
         </div>
         );
     }
-
 }
+//React Redux where I use both of these together
 
-//React Redux
+const mapStateToProps = (state) => { // this maps my redux state to props that react can use directly so use these props in react to get redux state
+    return {
+      input: state.input,
+      previewSwitch: state.previewSwitch,
+      editorSwitch: state.editorSwitch,
+    }
+  };
 
+const mapDispatchToProps = (dispatch) => { // since dispatch actions can change redux state this likewise gives props to react but this time for these dispatch actions
+return {                                    // using these lets me change redux state
+    loginAction: (message) => {
+        dispatch(loginAction(message));
+    },
+    previewToggle: () => {
+        dispatch(previewToggle());
+    },
+    editorToggle: () => {
+        dispatch(editorToggle());
+    }
+}
+}
 const Container = connect(mapStateToProps,mapDispatchToProps)(MarkDownContainer)
-
 class ActualMarkdown extends React.Component {
     constructor(props){
         super(props);
@@ -294,7 +230,5 @@ class ActualMarkdown extends React.Component {
            </Provider>
        );
     }
-    
 }
-
 ReactDom.render(<ActualMarkdown />, document.getElementById('root'));
